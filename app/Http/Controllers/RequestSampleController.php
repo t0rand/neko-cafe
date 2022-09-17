98d2fe900b40c66006beaddbd2872a66114f1263
t0rand <t0randbukky@gmail.com>
2022-09-17 23:32:48 +0900

    ログイン実装

--- app/Http/Controllers/RequestSampleController.php
+++ app/Http/Controllers/RequestSampleController.php
@@ -10,6 +10,10 @@ class RequestSampleController extends Controller
         return view('form');
     }
 
+    public function loginForm() {
+        return view('login');
+    }
+
     public function queryStrings(Request $request) {
         $keyword = $request->get('keyword', '未設定');
         return 'キーワードは「'.$keyword.'」です。';
@@ -21,5 +25,17 @@ class RequestSampleController extends Controller
 
     public function productsArchive(Request $request, $category, $year) {
         return 'category :'.$category.'<br>year :'.$year.'<br> page :'.$request->get('page', 1);
+    }
+
+    public function routeLink() {
+        $url = route('profile', ['id' => 1, 'photos' => 'yes']);
+        return 'プロフィールページのURLは'.$url;
+    }
+
+    public function login(Request $request) {
+        if ($request->get('email') === 'test@example.com' && $request->get('password') === '12345678') {
+        return 'ログイン成功';
+    }
+        return 'ログイン失敗';
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -30,22 +30,21 @@ Route::get('/', fn () => view('index'));
 Route::get('/curriculum', fn () => view('curriculum'));
 
 // 世界の時間
-Route::get('/world-time', [UtilityController::class, 'worldtime']
-);
+Route::get('/world-time', [UtilityController::class, 'worldtime']);
 
 // おみくじ
-Route::get('/omikuji', [GameController::class, 'omikuji']
-);
+Route::get('/omikuji', [GameController::class, 'omikuji']);
 
 // モンティ・ホール問題
-Route::get('/monty-hall', [GameController::class, 'montyHall']
-);
+Route::get('/monty-hall', [GameController::class, 'montyHall']);
 
 // リクエスト
-Route::get('/form', [RequestSampleController::class, 'form']
-);
-Route::get('/query-strings', [RequestSampleController::class, 'queryStrings']
-);
-Route::get('/users/{id}', [RequestSampleController::class, 'profile']
-);
+Route::get('/form', [RequestSampleController::class, 'form']);
+Route::get('/query-strings', [RequestSampleController::class, 'queryStrings']);
+Route::get('/users/{id}', [RequestSampleController::class, 'profile'])->name('profile');
 Route::get('/products/{category}/{year}', [RequestSampleController::class, 'productsArchive']);
+Route::get('/route-link', [RequestSampleController::class, 'routeLink']);
+
+// ログイン
+Route::get('/login', [RequestSampleController::class, 'loginForm']);
+Route::post('/login', [RequestSampleController::class, 'login'])->name('login');
