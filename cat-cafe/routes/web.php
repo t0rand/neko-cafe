aff1d7d81daa85fbc231b0c0682257cf378e300d
t0rand <qifumu.t0u@docomo.ne.jp>
2022-09-28 00:29:00 +0900

    お問い合わせフォーム、ルーティング追加。

--- cat-cafe/routes/web.php
+++ cat-cafe/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ContactController;
+use Illuminate\Routing\Route as RoutingRoute;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+// お問い合わせフォーム
+Route::get('/contact', [ContactController::class, 'index'])->name('contact');
+Route::post('/contact', [ContactController::class, 'sendMail']);
+Route::get('/contact/complete', [ContactController::class, 'complete'])->name('contact.complete');
